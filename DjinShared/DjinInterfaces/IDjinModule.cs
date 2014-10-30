4486abab05b9d97485edcab89fbd62ce46d15a08
VCharlie <victorcharlie@web.de>
2014-10-31 00:57:16 +0100

    Partially implemented the ModuleManager. The ModuleManager will create an instance of a given Class (Assembly) and run its Run()-Method in a seperate Thread.

---
+++ DjinShared/DjinInterfaces/IDjinModule.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Djin.Shared.Interfaces
+{
+
+    public interface IDjinModule
+    {
+        /* will only be called once,
+         * when the module is donwloaded
+         */
+        void Install();
+
+        /* will only be called once,
+         * when the module will be deleted
+         */
+        void Uninstall();
+
+        /* Will be called every time the
+         * instance of the
+         * Module was created
+         */
+        void OnStart();
+
+        /* Will be called every time
+         * an instance of the 
+         * Module will be destroyed
+         */
+        void OnStop();
+
+        /* This is the "real" code which will be executed.
+         * It will be called after OnStart() end before OnStop()
+         */
+        void Run();
+    }
+}
