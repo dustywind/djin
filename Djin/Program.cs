42c7e8ca464f63219a85f9eb2182d5ac247edb60
VCharlie <victorcharlie@web.de>
2014-10-30 15:36:46 +0100

    Initial commit

---
+++ Djin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+#if DEBUG
+                if (args[0].CompareTo("--test") == 0)
+                {
+                    Djin.Playground.Playground.Play();
+                }
+#endif
+            }
+            return;
+        }
+    }
+}
+
---
+++ DjinModules/DjinWebListener/WebListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djin.Modules.WebListener
+{
+    public class WebListener
+    {
+        public void Hello()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
