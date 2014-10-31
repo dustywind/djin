4e00978738e49ca277d1e6bb407a2f07ac0c9cb3
VCharlie <victorcharlie@web.de>
2014-10-31 10:03:57 +0100

    Added a sample module for testing purpose

--- Djin/DjinCore/Playground/CorePlayground.cs
+++ Djin/DjinCore/Playground/CorePlayground.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Djin.Core.ModuleManagement;
+
 namespace Djin.Core.Playground
 {
     class CorePlayground
     {
         public void Play()
         {
+            var mm = new ModuleManager();
+            var mdesc = new ModuleDescription();
 
+            mdesc.Path = @"E:\Projects\djin\DjinModules\DjinWebListener\bin\Debug\DjinModuleTest.dll";
         }
     }
 }
--- DjinModules/DjinModuleTest/DjinModuleTest.cs
+++ DjinModules/DjinModuleTest/DjinModuleTest.cs
@@ -4,33 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Djin.Modules.WebListener
+namespace Djin.Modules.DjinModuleTest
 {
-    public class WebListener : global::Djin.Shared.Interfaces.IDjinModule
+    public class DjinModuleTest : global::Djin.Shared.Interfaces.IDjinModule
     {
+
         public void Install()
         {
+            Console.WriteLine("DjinModuleTest.Install");
             return;
         }
 
         public void Uninstall()
         {
+            Console.WriteLine("Djin.ModuleTest.Uninstall");
             return;
         }
 
         public void OnStart()
         {
+            Console.WriteLine("Djin.OnStart");
             return;
         }
 
         public void Run()
         {
+            Console.WriteLine("Djin.Run");
             return;
         }
 
         public void OnStop()
         {
+            Console.WriteLine("Djin.OnStop");
             return;
         }
+
     }
 }
