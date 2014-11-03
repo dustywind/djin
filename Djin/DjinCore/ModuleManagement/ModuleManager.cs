94691dcfcb4983bd8693883e01ce1c0506dad560
VCharlie <victorcharlie@web.de>
2014-11-03 14:30:24 +0100

    ModuleManager is now a singleton.

--- Djin/DjinCore/ModuleManagement/ModuleManager.cs
+++ Djin/DjinCore/ModuleManagement/ModuleManager.cs
@@ -13,66 +13,46 @@ namespace Djin.Core.ModuleManagement
      * starting modules and keeping track of them.
      * For each Instance of a Module a new Thread will be started
      */
-    class ModuleManager
+    class ModuleManager : AbstractModuleManager
     {
 
         private Dictionary<ModuleDescription, IDjinModule> LoadedModuleInstances = new Dictionary<ModuleDescription,IDjinModule>();
-        private Dictionary<ModuleDescription, ModuleThread> RuningModuleInstances = new Dictionary<ModuleDescription,ModuleThread>();
+        private Dictionary<ModuleDescription, ModuleThread> RunningModuleInstances = new Dictionary<ModuleDescription,ModuleThread>();
 
         private static object Lock = new object();
 
-        internal ModuleManager() { }
-
-        private ModuleThread GetRunningModule(ModuleDescription description)
+        private static ModuleManager _Instance = null;
+        internal static ModuleManager Instance
         {
-            return RuningModuleInstances[description];
-        }
-
-        private IDjinModule GetLoadedModule(ModuleDescription description)
-        {
-            return LoadedModuleInstances[description];
-        }
-
-
-        private bool ModuleAlreadyRunning(ModuleDescription description)
-        {
-            return RuningModuleInstances.ContainsKey(description);
-        }
-
-        private bool ModuleAlreadyLoaded(ModuleDescription description)
-        {
-            return LoadedModuleInstances.ContainsKey(description);
-        }
-
-
-        internal void AddAndRunModule(ModuleDescription description)
-        {
-            AddModule(description);
-            RunLoadedModule(description);
-        }
-
-        internal void AddModule(ModuleDescription description)
-        {
-                try
+            get
+            {
+                if (_Instance == null)
                 {
-                    lock (ModuleManager.Lock)
+                    _Instance = new ModuleManager();
+                }
+                return _Instance;
+            }
+        }
+
+        private ModuleManager() { }
+
+        override internal void AddModule(ModuleDescription description)
+        {
+            try
+            {
+                lock (ModuleManager.Lock)
+                {
+                    if (LoadedModuleInstances.ContainsKey(description) == false)
                     {
-                        if (LoadedModuleInstances.ContainsKey(description) == false)
-                        {
-                            var moduleInstance = (IDjinModule)new InstanceCreator(description).GetNewInstance();
-                            AddModule(description, moduleInstance);
-                        }
+                        var moduleInstance = (IDjinModule)new InstanceCreator(description).GetNewInstance();
+                        AddModule(description, moduleInstance);
                     }
                 }
-                catch (Exception e) { throw e; }
+            }
+            catch (Exception e) { throw e; }
         }
 
-        private void AddModule(ModuleDescription description, IDjinModule instance)
-        {
-            LoadedModuleInstances.Add(description, instance);
-        }
-
-        internal void RunLoadedModule(ModuleDescription description)
+        override internal void RunLoadedModule(ModuleDescription description)
         {
             try
             {
@@ -80,10 +60,10 @@ namespace Djin.Core.ModuleManagement
 
                 lock (ModuleManager.Lock)
                 {
-                    if (RuningModuleInstances.ContainsKey(description) == false)
+                    if (RunningModuleInstances.ContainsKey(description) == false)
                     {
-                        var thread = new ModuleThread(instance);
-                        RuningModuleInstances.Add(description, thread);
+                        var thread = new ModuleThread(description, instance);
+                        RunningModuleInstances.Add(description, thread);
                         thread.Start();
                     }
                 }
@@ -92,6 +72,50 @@ namespace Djin.Core.ModuleManagement
             {
                 throw new Exception("Requested instance of: " + description + " hasn't been loaded yet.");
             }
+        }
+
+        internal override void HaltLoadedModule(ModuleDescription description)
+        {
+            lock (ModuleManager.Lock)
+            {
+                if (RunningModuleInstances.ContainsKey(description) == false)
+                {
+                    // nothing to do
+                    return;
+                }
+                ModuleThread mt = RunningModuleInstances[description];
+            }
+        }
+
+        override internal void AddAndRunModule(ModuleDescription description)
+        {
+            AddModule(description);
+            RunLoadedModule(description);
+        }
+
+        private ModuleThread GetRunningModule(ModuleDescription description)
+        {
+            return RunningModuleInstances[description];
+        }
+
+        private IDjinModule GetLoadedModule(ModuleDescription description)
+        {
+            return LoadedModuleInstances[description];
+        }
+
+        private bool ModuleAlreadyRunning(ModuleDescription description)
+        {
+            return RunningModuleInstances.ContainsKey(description);
+        }
+
+        private bool ModuleAlreadyLoaded(ModuleDescription description)
+        {
+            return LoadedModuleInstances.ContainsKey(description);
+        }
+
+        private void AddModule(ModuleDescription description, IDjinModule instance)
+        {
+            LoadedModuleInstances.Add(description, instance);
         }
 
     }
