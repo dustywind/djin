d77e10e4594b69e55dc9f1f472983a125200f5e3
VCharlie <victorcharlie@web.de>
2014-12-19 12:09:21 +0100

    Moved the SuicideCollector from the ModuleThread.cs in its very own file.

--- Djin/DjinCore/ModuleManagement/ModuleThread.cs
+++ Djin/DjinCore/ModuleManagement/ModuleThread.cs
@@ -14,19 +14,21 @@ namespace Djin.Core.ModuleManagement
     {
         private IDjinModule ModuleInstance;
         private ModuleDescription Description;
-        private bool Loop;
+        private volatile bool Loop;
 
         private Thread ThreadInstance;
-        private int MillisecondTimeout = 500;
+        private const int MillisecondTimeout = 500;
 
         private static SuicideCollector collector = SuicideCollector.Instance;
 
         internal ModuleThread(ModuleDescription description, IDjinModule moduleInstance)
         {
-            this.ModuleInstance = moduleInstance;
-            this.Description = description;
-            this.Loop = Description.Loop;
-            ThreadInstance = new Thread(new ThreadStart(this.ThreadProcessing));
+            ModuleInstance = moduleInstance;
+            Description = description;
+            Loop = Description.Loop;
+            ThreadInstance = new Thread(new ThreadStart(this.ThreadProcessing)){
+                Name = description.ToString()
+            };
         }
 
         internal void Start()
@@ -34,13 +36,9 @@ namespace Djin.Core.ModuleManagement
             ThreadInstance.Start();
         }
 
-        internal void Stop()
+        internal void Stop(int milliSeconds=MillisecondTimeout)
         {
-            Stop(MillisecondTimeout);
-        }
-
-        internal void Stop(int milliSeconds)
-        {
+            Loop = false;
             if (ThreadInstance.Join(milliSeconds) == false)
             {
                 ThreadInstance.Abort();
@@ -59,7 +57,7 @@ namespace Djin.Core.ModuleManagement
                     ModuleInstance.OnStop();
                 } while (this.Loop);
 
-                // let the modulemanager know that the Thread want's to exit.
+                // let the modulemanager know that the Thread wants to exit.
                 this.Suicide();
             }
             catch (ThreadAbortException) // Thread-execution was aborted
@@ -80,95 +78,5 @@ namespace Djin.Core.ModuleManagement
         }
     }
 
-    class SuicideCollector : IDisposable
-    {
-        /**
-         * The SuicideCollector will tell the ModuleManager
-         * which DjinModule wants to be joined.
-         *  
-         * How it works:
-         * SuicideCollector will create a Thread which checks the 
-         * CorpseCollection-queue to gather DjinModules that terminated
-         * For each DjinModule in CorpseCollection the ModuleManager will be
-         * called to halt the given DjinModule
-         */
-        private static SuicideCollector _instance = null;
-        internal static SuicideCollector Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = new SuicideCollector();
-                }
-                return _instance;
-            }
-        }
-
-        private BlockingCollection<ModuleDescription> CorpseCollection = new BlockingCollection<ModuleDescription>();
-        private Thread Looper;
-
-        private bool _Shutdown = false;
-        private int LooperTimeout = 500;
-        private int LoopInterval = 200;
-
-        internal SuicideCollector()
-        {
-            Looper = new Thread(new ThreadStart(this.SuicideCollectLoop));
-            Looper.Start();
-        }
-
-        internal void Stop()
-        {
-            Shutdown();
-            ShutdownLooper();
-        }
-
-        internal void Suicide(ModuleDescription description)
-        {
-            if (WillShutdown())
-            {
-                CorpseCollection.Add(description);
-            }
-        }
-
-        internal void SuicideCollectLoop()
-        {
-            while( WillShutdown() == false) {
-                ModuleDescription killMe = null;
-                if (CorpseCollection.TryTake(out killMe, LoopInterval))
-                {
-                    ModuleManager.Instance.HaltLoadedModule(killMe);
-                }
-
-            }
-        }
-
-        private void Shutdown()
-        {
-            this._Shutdown = true;
-        }
-
-        private bool WillShutdown()
-        {
-            return this._Shutdown;
-        }
-
-        private void ShutdownLooper()
-        {
-            if (Looper.Join(LooperTimeout) == false)
-            {
-                Looper.Abort();
-            }
-
-        }
-
-        void IDisposable.Dispose()
-        {
-            // TODO
-            // free BlockingQCollection
-            CorpseCollection.Dispose();
-            throw new NotImplementedException();
-        }
-    }
+    
 }
---
+++ Djin/DjinCore/ModuleManagement/SuicideCollector.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Djin.Core.ModuleManagement
+{
+    class SuicideCollector : IDisposable
+    {
+        /**
+         * The SuicideCollector will tell the ModuleManager
+         * which DjinModule wants to be joined.
+         *  
+         * How it works:
+         * SuicideCollector will create a Thread which checks the 
+         * CorpseCollection-queue to gather DjinModules that terminated
+         * For each DjinModule in CorpseCollection the ModuleManager will be
+         * called to halt the given DjinModule
+         */
+        private static SuicideCollector _instance = null;
+        internal static SuicideCollector Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SuicideCollector();
+                }
+                return _instance;
+            }
+        }
+
+        private BlockingCollection<ModuleDescription> CorpseCollection = new BlockingCollection<ModuleDescription>();
+        private Thread Looper;
+
+        private bool _Shutdown = false;
+        private int LooperTimeout = 500;
+        private int LoopInterval = 200;
+
+        internal SuicideCollector()
+        {
+            Looper = new Thread(new ThreadStart(this.SuicideCollectLoop));
+            Looper.Start();
+        }
+
+        internal void Stop()
+        {
+            Shutdown();
+            ShutdownLooper();
+        }
+
+        internal void Suicide(ModuleDescription description)
+        {
+            if (WillShutdown())
+            {
+                CorpseCollection.Add(description);
+            }
+        }
+
+        internal void SuicideCollectLoop()
+        {
+            while (WillShutdown() == false)
+            {
+                ModuleDescription killMe = null;
+                if (CorpseCollection.TryTake(out killMe, LoopInterval))
+                {
+                    ModuleManager.Instance.HaltLoadedModule(killMe);
+                }
+
+            }
+        }
+
+        private void Shutdown()
+        {
+            this._Shutdown = true;
+        }
+
+        private bool WillShutdown()
+        {
+            return this._Shutdown;
+        }
+
+        private void ShutdownLooper()
+        {
+            if (Looper.Join(LooperTimeout) == false)
+            {
+                Looper.Abort();
+            }
+
+        }
+
+        void IDisposable.Dispose()
+        {
+            // TODO
+            // free BlockingQCollection
+            CorpseCollection.Dispose();
+            throw new NotImplementedException();
+        }
+    }
+}
