5043c09fa1b5d60ecd2c8cec9b0bdbeab6177be6
VCharlie <victorcharlie@web.de>
2014-12-20 10:41:54 +0100

    Added a new library. This Library takes some tasks and will solve them multithreaded with a given number of Threads. If there are more Tasks than threads, the tasks will be stored in a queue.

---
+++ DjinShared/Library/DjinThreadPool/ThreadPool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djin.Shared
+{
+    public class ThreadPool
+    {
+        public void AddToQueu(IThreadPoolWorkPackage workPackage)
+        {
+
+        }
+    }
+
+    public class IThreadPoolWorkPackage
+    {
+        public void Work();
+    }
+}
